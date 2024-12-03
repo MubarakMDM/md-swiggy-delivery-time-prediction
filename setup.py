041257a4387d85d97d37210c7db824dda91451a2
MubarakMDM <md93150@gmail.com>
2024-12-03 18:33:27 +0100

    initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Swiggy delivery time prediction',
+    author='Mubar1',
+    license='',
+)
